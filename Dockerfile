45a17c56394aae4f770b59774841b4059516869c
pakidaxed <jucius.tomas@gmail.com>
2020-10-29 02:00:00 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM php:7.4-fpm-alpine
 RUN apk update \
     && apk add --no-cache composer
 RUN docker-php-ext-install pdo pdo_mysql
+RUN composer install
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig', []);
+    }
+
+}
